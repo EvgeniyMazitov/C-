780f593f2ceeea07e761c18e5476da9a54776067
Evgeniy <>
2022-06-23 21:45:17 +0300

    add Task5 new var

--- Seminar1/Task5/Program.cs
+++ Seminar1/Task5/Program.cs
@@ -4,6 +4,17 @@
 456 -> 6   782 -> 2   918 -> 8
 */
 
+Console.WriteLine("Введите число");
+string NumberUser = Console.ReadLine();
+Console.WriteLine(NumberUser[NumberUser.Length-1]);
+
+/*
+char q = 'f';
+
+char [] temp = new char [3]
+
+
+
 Console.WriteLine("Введите трехзначное число:");
 string NumberUser = Console.ReadLine();
 int number = int.Parse(NumberUser);
@@ -14,3 +25,5 @@ if (number > 99 & number <1000)
 }
 else
     Console.WriteLine("Число введено с ошибкой");
+
+*/
