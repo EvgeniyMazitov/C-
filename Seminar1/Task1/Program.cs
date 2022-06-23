6ce65d803035e05f5a2128025f0a189dc3ceb47d
Evgeniy <>
2022-06-23 20:34:02 +0300

    add Task1

---
+++ Seminar1/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите программу, которая на входе принимает число и выдает его квадрат (число умноженное на само себя)
+*/
+
+Console.WriteLine("Введите число, которое хотите возвести в квадрат");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+
+int result = number * number;
+
+//Console.WriteLine("Квадрат числа {1} равняется {2}", number, result);
+
+Console.WriteLine($"Квадрат числа {number} равняется {result}");
+
+//Console.WriteLine($"Квадрат числа" + number "равняется" + result);
