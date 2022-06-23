acc50b7a39e275dd8bd0380f1990f286bf9aebb0
Evgeniy <>
2022-06-23 21:40:55 +0300

    add Task5

---
+++ Seminar1/Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+7. Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает последнюю цифру этого числа.
+456 -> 6   782 -> 2   918 -> 8
+*/
+
+Console.WriteLine("Введите трехзначное число:");
+string NumberUser = Console.ReadLine();
+int number = int.Parse(NumberUser);
+
+if (number > 99 & number <1000)
+{
+    Console.WriteLine($"Последняя цифра {number % 10}");
+}
+else
+    Console.WriteLine("Число введено с ошибкой");
