a592f6c5c0f4f11afcbb13caea303bc09e94b69c
Evgeniy <>
2022-06-24 15:32:56 +0300

    add Task6

--- Seminar1_Home_Work/Task6_If_Even_Number/Program.cs
+++ Seminar1_Home_Work/Task6_If_Even_Number/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+(делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.Write("Введите число:");
+string FirstNumberUser = Console.ReadLine();
+bool FirstNumberTest = int.TryParse(FirstNumberUser, out int FirstNumber);
+
+if  (FirstNumberTest == false)
+    Console.WriteLine("Введено не число");
+else
+    {
+       if (FirstNumber % 2 == 0) 
+        Console.WriteLine($"Число {FirstNumber} является четным");
+
+        else
+            Console.WriteLine($"Число {FirstNumber} является нечетным");
+    }
