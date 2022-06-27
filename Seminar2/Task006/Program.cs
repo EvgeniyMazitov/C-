c0c7c48f7ba57111ea3904fa81b5aaade20f9008
Evgeniy <>
2022-06-27 21:21:22 +0300

    add task006 new

--- Seminar2/Task006/Program.cs
+++ Seminar2/Task006/Program.cs
@@ -8,10 +8,47 @@
 
 https://docs.microsoft.com/ru-ru/dotnet/csharp/
 
-*/
+
 
 Random rnd = new Random();
 
 double result = Math.Pow(2,10);
 
 Math.Abs(-4);
+
+int number = 10;
+InsertToDb(number);
+Console.WriteLine(number);
+
+MyClass Item = new MyClass
+{
+    DisplayName = "3232333",
+    Id = 120
+};
+
+InsertToDb(item);
+
+Console.WriteLine($"DisplaName: {item.DisplayName} ID: {item.Id}");
+
+void SomeFunction(MyClass item)
+{
+    //требуха, которая не вернет значений, а просто выполнится
+
+    item.DisplayName = null;
+    item.id = 0;
+}
+
+String AnotherFunction()
+{
+    return string.Empty;
+}
+*/
+
+int number = 1;
+SomeFunction(ref number);
+Console.WriteLine(number);
+
+void SomeFunction(ref int number)
+{
+    number = 12;
+}
