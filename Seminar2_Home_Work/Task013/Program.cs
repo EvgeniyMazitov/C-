b9338993c7aff4baef11b01c6fd326e1b7e0ab0a
Evgeniy <>
2022-06-28 20:44:19 +0300

    add task013

--- Seminar2_Home_Work/Task013/Program.cs
+++ Seminar2_Home_Work/Task013/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+Console.Write("Введите число :");
+string numberUser = Console.ReadLine();
+bool numberTest = int.TryParse(numberUser, out int number);
+
+if (numberTest == false)
+    Console.WriteLine("Введено не число");
+else if (Math.Abs(number) < 100)
+    Console.WriteLine("Третьей цифры числа нет");
+else
+{
+    Console.WriteLine($"Третья цифра числа = {numberUser[2]}");
+}
