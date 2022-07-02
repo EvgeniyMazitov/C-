7dce24e961a4d169a8e920a87124a344cc5ae803
Evgeniy <>
2022-07-02 17:21:11 +0300

    add lection examples

--- Lection3/Examples001_Method/Program.cs
+++ Lection3/Examples001_Method/Program.cs
@@ -1,12 +1,22 @@
-﻿
+﻿// Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
 
+//Method1();
+
+// Вид 2
 void Method2(string msg)
 {
     Console.WriteLine(msg);
 }
-Method2(msg: "Текст сообщения");
 
-void Method(string msg, int count)
+//Method2("Текст сообщения");
+// Ниже вариант вызова, если нужно указать конкретные аргументы при вызове,нужно, если аргументов больше, чем 1
+//Method2(msg: "Текст сообщения");
+
+void Method21(string msg, int count)
 {
     int i = 0;
     while (i < count)
@@ -15,8 +25,12 @@ void Method(string msg, int count)
         i++;
     }
 }
-//Method21(msg: "Текст", count: 4);
-Method21(count: 4, msg: "Текст");
+//Method21("Текст", 4);
+
+//пример явного указания вводимых аргументов ниже
+//Method21(count: 4, msg: "Текст");
+
+// Вид3
 
 int Method3()
 {
@@ -26,18 +40,21 @@ int Method3()
 int year = Method3();
 //Console.WriteLine(year);
 
-int Method4(int count, char c)
+// Вид4
+
+string Method4(int count, string text)
 {
     int i = 0;
+    //string result = ""; - так можно, но это не комильфо, ниже лучше
     string result = string.Empty;
 
-while (i<count)
-{
-    result = result + text;
-    i++;
-}
-return result;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
 }
 
-string res=Method4(10, "z");
+string res = Method4(10, "z");
 Console.WriteLine(res);
