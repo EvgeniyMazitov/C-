e207a83737611943b41fdb858454836e5630f358
Evgeniy <>
2022-07-04 20:39:57 +0300

    add ask001, task002

--- Seminar4/Task002/Program.cs
+++ Seminar4/Task002/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+
+*/
+
+//метод, который возвращает число из консоли
+// Метод возвращает число из консоли
+
+
+string GetNumber(string message)
+{
+    int result = 0;
+    string num = string.Empty;
+    string errorMessage = "Вы ввели не число. Введите корректное число.";
+
+    while(true)
+    {
+        
+        Console.WriteLine(message);
+        num = Console.ReadLine();
+
+        if(int.TryParse(num, out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine(errorMessage);
+        }
+    }
+
+    return num;
+}
+
+string number = GetNumber("Введите число:");
+Console.WriteLine($"Число {number} имеет {number.Length} знаков.");
