79abe683cc99e54b0b299abf050e2285bd413e7d
Evgeniy <>
2022-07-07 21:13:56 +0300

    add task004

--- Seminar5/Task004/Program.cs
+++ Seminar5/Task004/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 35: Задайте одномерный массив из 123 случайных чисел. 
+Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5
+
+*/
+
+int[] GetArray(int dimension)
+{
+    int[] arr = new int[dimension];
+    Random randomizer = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = randomizer.Next(0, 124);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int CountElements(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i]<=99)
+            count ++;
+    }
+
+    return count;
+}
+
+
+Console.WriteLine("Введите размерность массива :");
+int dimension = int.Parse(Console.ReadLine());
+
+int[] array = GetArray(dimension);
+PrintArray(array);
+
+int result = CountElements(array);
+
+Console.WriteLine();
+Console.WriteLine($"Количество чисел на отрезке [10;99] {result}");
