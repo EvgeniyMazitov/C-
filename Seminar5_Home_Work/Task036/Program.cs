f401115787cb6dd8a0114742cd221a0166f7a008
Evgeniy <>
2022-07-08 15:52:22 +0300

    add task036

--- Seminar5_Home_Work/Task036/Program.cs
+++ Seminar5_Home_Work/Task036/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] GetArray(int dimension)
+{
+    int[] arr = new int[dimension];
+    Random randomizer = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = randomizer.Next(-99, 100);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int OddNumbersSum(int[] arr)
+{
+    int sum = 0;
+
+    for (int i = 1; i < arr.Length; i+=2)
+    {
+        sum=sum+arr[i];
+    }
+
+    return sum;
+}
+
+Console.WriteLine("Введите размерность массива:");
+int dimension = int.Parse(Console.ReadLine());
+
+int[] array = GetArray(dimension);
+PrintArray(array);
+
+int sum = OddNumbersSum(array);
+
+Console.WriteLine();
+Console.WriteLine($"Сумма нечетных элементов массива: {sum}");
