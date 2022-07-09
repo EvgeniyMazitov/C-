42d811d594be3224b2ae7a6cfb0fe275452f6622
Evgeniy <>
2022-07-09 13:20:01 +0300

    add examples 1-4

---
+++ Lection4/Example003/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Вычисление факториала через рекурсию
+*/
+
+double Factorial(double n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+
+for (int i = 1; i < 40; i++)
+{
+   Console.WriteLine($"{i}!= {Factorial(i)}");
+}
