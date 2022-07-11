c8806d1e05cd58aaa9f0e7e8058e1a78164fff97
Evgeniy <>
2022-07-11 21:09:35 +0300

    add  001,002,003

--- Seminar6/Task002/Program.cs
+++ Seminar6/Task002/Program.cs
@@ -1,6 +1,55 @@
 ﻿/*
-Задача 38: Задайте массив вещественных чисел.
-Найдите разницу между максимальным и минимальным элементов массива.
+Задача 40: Напишите программу, которая принимает на вход три числа и проверяет,
+может ли существовать треугольник с сторонами такой длины.
 
-[3 7 22 2 78] -> 76
+Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон
 */
+
+int GetNumber(string message)
+{
+    int result = 0;
+    string errorMessage = "Вы ввели не число. Введите корректное число.";
+
+    while (true)
+    {
+
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+
+            break;
+        else
+        {
+            Console.Clear();
+            Console.WriteLine(errorMessage);
+        }
+    }
+
+    return result;
+}
+
+int Multi(int number1, int number2)
+{
+    int result = 1;
+    for (int i = 1; i <= number2; i++)
+    {
+        result = result * number1;
+    }
+    return result;
+}
+
+void Triangle(int a, int b, int c)
+{
+    if (a < b + c && b < a + c && c < a + b)
+        Console.WriteLine($"Треугольник со сторонами a={a}, b={b}, c={c} существует");
+    else
+        Console.WriteLine($"Треугольник со сторонами a={a}, b={b}, c={c} не существует");
+
+
+}
+
+int a = GetNumber("Введите размер первой стороны: ");
+int b = GetNumber("Введите размер второй стороны: ");
+int c = GetNumber("Введите размер третьей стороны: ");
+
+Triangle(a, b, c);
