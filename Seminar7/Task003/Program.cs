5c01fe4f93ae047f230834bd8d4fde6df7fbf338
Evgeniy <>
2022-07-14 20:31:57 +0300

    add  Task001,002,003

---
+++ Seminar7/Task003/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные,
+и замените эти элементы на их квадраты.
+Например, изначально массив
+ выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+Новый массив будет выглядеть 
+вот так:
+1 4 7 2
+5 81 2 9
+8 4 2 4
+*/
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random randomizer = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = randomizer.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeMatrix(int[,] matrix)
+{
+    for (int i = 2; i < matrix.GetLength(0); i+=2)
+    {
+        for (int j = 2; j < matrix.GetLength(1); j+=2)
+        {
+ //           if (( % 2 = 0 && j % 2 = 0 && i!= 0 && j != 0)
+                matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+    }
+}
+
+Console.WriteLine("Введите число m:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = int.Parse(Console.ReadLine());
+
+int[,] matrix = InitMatrix(m, n);
+
+Console.WriteLine($"Матрица размером {m}x{n}:");
+Console.WriteLine();
+
+PrintMatrix(matrix);
+Console.WriteLine();
+ChangeMatrix(matrix);
+PrintMatrix(matrix);
