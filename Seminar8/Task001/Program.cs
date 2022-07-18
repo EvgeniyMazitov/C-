15120a1112facd144d39c0da76a78f110fdeaff9
Evgeniy <>
2022-07-18 21:50:12 +0300

    add  task001,002,003,004

--- Seminar8/Task001/Program.cs
+++ Seminar8/Task001/Program.cs
@@ -1,2 +1,102 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 53: Задайте двумерный массив. Напишите программу, 
+которая поменяет местами первую и последнюю строку массива.
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    string errorMessage = "Вы ввели не число. Введите корректное число.";
+
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+            break;
+        else
+        {
+            Console.Clear();
+            Console.WriteLine(errorMessage);
+        }
+    }
+    return result;
+}
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random randomizer = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = randomizer.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangStringMatrix(int[,] matrix)
+{
+    int buffer = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        buffer = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0)-1, j];
+        matrix[matrix.GetLength(0)-1, j] = buffer;
+    }
+}
+
+int m = GetNumber("Введите число строк массива m:");
+int n = GetNumber("Введите число столбцов массива n:");
+
+int[,] matrix = InitMatrix(m, n);
+Console.WriteLine();
+Console.WriteLine($"Матрица размером {m}x{n}:");
+Console.WriteLine();
+
+PrintMatrix(matrix);
+Console.WriteLine();
+ChangStringMatrix(matrix);
+PrintMatrix(matrix);
+
+/*
+
+int[,] GetNewArray(int[,] matrix)
+{       
+  int[,] array = new int[matrix.GetLength(0), matrix.GetLength(1)];
+
+  for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i == 0)
+            {
+                array[i, j] = matrix[matrix.GetLength(0) - 1, j]; 
+            }
+            else if(i == matrix.GetLength(0) - 1)
+            {
+                array[i, j] = matrix[0, j];
+            }
+            else
+            {
+                array[i, j] = matrix[i, j];
+            }
+        }
+    }
+  return array;  
+}
+*/
--- Seminar8/Task004/Program.cs
+++ Seminar8/Task004/Program.cs
@@ -1,2 +1,116 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 59: Задайте двумерный массив из целых чисел. Напишите программу,
+которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Наименьший элемент - 1, на выходе получим 
+следующий массив:
+9 4 2
+2 2 6
+3 4 7
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    string errorMessage = "Вы ввели не число. Введите корректное число.";
+
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+            break;
+        else
+        {
+            Console.Clear();
+            Console.WriteLine(errorMessage);
+        }
+    }
+    return result;
+}
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random randomizer = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = randomizer.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void DelColumnsRowsMatrix(int[,] matrix)
+{
+    List<int> minI = new List<int>();
+    List<int> minJ = new List<int>();
+    int min = matrix[0, 0];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < min)
+            {
+                min = matrix[i, j];
+            }
+        }
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] == min)
+            {
+                minI.Add(i);
+                minJ.Add(j);
+            }
+        }
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        if (minI.Contains(i))
+        {
+            continue;
+        }
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (minJ.Contains(j))
+            {
+                continue;
+            }
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = GetNumber("Введите число строк массива m:");
+int n = GetNumber("Введите число столбцов массива n:");
+
+int[,] matrix = InitMatrix(m, n);
+Console.WriteLine();
+Console.WriteLine($"Матрица размером {m}x{n}:");
+Console.WriteLine();
+
+PrintMatrix(matrix);
+Console.WriteLine();
+DelColumnsRowsMatrix(matrix);
