5f82dc2c937ef28dc8e23c33bba5550e0f65a9de
Evgeniy <>
2022-07-19 16:17:25 +0300

    add task054

--- Seminar8_Home_Work/Task054/Program.cs
+++ Seminar8_Home_Work/Task054/Program.cs
@@ -13,5 +13,87 @@
 1 2 4 7
 2 3 5 9
 2 4 4 8
+
+Решение выполнено по тексту задания
+(в тест-кейсе выполнена сортировка по возрастанию, а не по убыванию)
 */
 
+int GetNumber(string message)
+{
+    int result = 0;
+    string errorMessage = "Вы ввели не число. Введите корректное число.";
+
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+            break;
+        else
+        {
+            Console.Clear();
+            Console.WriteLine(errorMessage);
+        }
+    }
+    return result;
+}
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random randomizer = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = randomizer.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            int maxPosition = j;
+            for (int k = j + 1; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, k] > matrix[i, maxPosition])
+                    maxPosition = k;
+            }
+            int buff = matrix[i, j];
+            matrix[i, j] = matrix[i, maxPosition];
+            matrix[i, maxPosition] = buff;
+        }
+    }
+}
+
+int m = GetNumber("Введите число строк массива m:");
+int n = GetNumber("Введите число столбцов массива n:");
+
+int[,] matrix = InitMatrix(m, n);
+
+Console.WriteLine();
+Console.WriteLine($"Матрицы размером {m}x{n}:");
+Console.WriteLine();
+PrintMatrix(matrix);
+Console.WriteLine();
+SortMatrix(matrix);
+Console.WriteLine("Отсортированная по убыванию в каждой строке матрица");
+Console.WriteLine();
+PrintMatrix(matrix);
